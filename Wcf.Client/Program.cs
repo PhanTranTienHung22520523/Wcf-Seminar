1e2c5a7b92cb26d618c31d0af71939da700c2240
PhanTranTienHung22520523 <22520523@gm.uit.edu.vn>
2024-11-01 16:32:30 +0700

    Add project files.

---
+++ Wcf.Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Wcf.Client
+{
+	[ServiceContract]
+	public interface IMessageService
+	{
+		[OperationContract]
+		string[] GetMessage();
+	}
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadLine();
+			string url = "net.tcp://localhost/6565/MessageService";
+			NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+			var channel=new ChannelFactory<IMessageService>(binding);
+			var endpoint = new EndpointAddress(url);
+			var proxy=channel.CreateChannel(endpoint);
+			var result = proxy?.GetMessage();
+			if (result != null)
+			{
+				result.ToList().ForEach(p => { Console.WriteLine(p); });
+			}
+			Console.ReadLine() ;
+		}
+	}
+}
